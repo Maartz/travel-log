31c27515efc5e7b98bb8032fd45107905c9addb2
maartz <maartz@protonmail.com>
2020-05-18 16:05:49 +0200

    Add .DOTENV | Import mongoose | Setup logs

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const middlewares = require('./middlewares');
+const logs = require('./api/logs');
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 app.use(morgan('combined'));
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN,
 }));
 
 app.get('/', (req, res) => {
@@ -16,6 +26,8 @@ app.get('/', (req, res) => {
     message: 'Hello world!',
   });
 });
+
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
