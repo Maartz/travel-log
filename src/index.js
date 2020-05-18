a47f643dade21419334845f1a112e518b59fe6aa
maartz <maartz@protonmail.com>
2020-05-18 12:23:42 +0200

    Get 404 and 500 middlewares | simple JSON endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
+app.use(morgan('combined'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// Error middlewares need 4 params.
+// We don't use next so need to mute the linter here.
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🛑 🛑 🛑' : error.stack,
+  });
+});
 
 const PORT = process.env.PORT || 1337;
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`🚀🚀 Listening on port: ${PORT}`);
-})
+});
